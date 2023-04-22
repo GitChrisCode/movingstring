c0256c1c0c3c9bef881d3221cbe0245d1a311eaa
Christian Hardt <christian.hardt@posteo.de>
2023-04-22 15:48:12 +0200

    added Method verschobenerString

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -2,6 +2,28 @@ package de.neuefische;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("Tagchen");
     }
+
+    public static String verschobenerString(String eingabe) {
+        StringBuilder ausgabe = new StringBuilder();
+        for (int i = 0; i < eingabe.length(); i++) {
+            char buchstabe = eingabe.charAt(i);
+            if (Character.isLetter(buchstabe)) {
+                char verschobenerBuchstabe = (char) (buchstabe + 3);
+                if ((Character.isLowerCase(buchstabe) && verschobenerBuchstabe > 'z')
+                        || (Character.isUpperCase(buchstabe) && verschobenerBuchstabe > 'Z')) {
+                    verschobenerBuchstabe -= 26;
+                }
+                ausgabe.append(verschobenerBuchstabe);
+            } else {
+                ausgabe.append(buchstabe);
+            }
+        }
+        return ausgabe.toString();
+    }
+
+
 }
---
+++ src/test/java/de/neuefische/MainTest.java
@@ -0,0 +1,13 @@
+package de.neuefische;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main_Test() {
+
+    }
+}
